1b054ef68c618eef1dea54109d39853e02d0263e
jing <98076350+jing2003@users.noreply.github.com>
2026-07-01 21:22:43 -1000

    Create routes for trips

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import tripRoutes from "./routes/trips.js";
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.get("/", (req, res) => {
       '<h1 style="text-align: center; margin-top: 50px;">✈️ OnTheFly API</h1>',
     );
 });
+
+app.use("/trips", tripRoutes);
 
 const PORT = process.env.PORT || 3001;
 
